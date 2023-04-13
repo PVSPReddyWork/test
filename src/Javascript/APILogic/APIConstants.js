87819613e0616722decdb1e41fffcb2ccc0e62b8
PVSPReddyWork <120256741+PVSPReddyWork@users.noreply.github.com>
2023-04-13 22:09:34 +0530

    Update APIConstants.js

--- src/Javascript/APILogic/APIConstants.js
+++ src/Javascript/APILogic/APIConstants.js
@@ -3,5 +3,5 @@ export const baseURL = 'https://esp8266apis.onrender.com';
 //export const ss = 'InsertDeviceConfigToDB';
 //export const DeleteDeviceConfig = 'DeleteDeviceConfigFromDB';
 
-export const GetDeviceConfigEndPoint = 'GetDeviceConfigFromDB';
-export const UpdateDeviceConfigEndPoint = 'UpdateDeviceConfigInDB';
+export const GetDeviceConfigEndPoint = '/GetDeviceConfigFromDB';
+export const UpdateDeviceConfigEndPoint = '/UpdateDeviceConfigInDB';
