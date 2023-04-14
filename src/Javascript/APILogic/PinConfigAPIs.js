541713b322adfb76889e8f0c9fca08cfa66338fc
PVSPReddyWork <120256741+PVSPReddyWork@users.noreply.github.com>
2023-04-14 13:01:40 +0530

    Update PinConfigAPIs.js

--- src/Javascript/APILogic/PinConfigAPIs.js
+++ src/Javascript/APILogic/PinConfigAPIs.js
@@ -1,12 +1,15 @@
 import { CustomLogger } from './../Modules/CustomLogger.js';
 import * as Constants from './APIConstants';
-const DataBaseConnection = require('./../Modules/DataBaseConnection.js');
+import {
+  GetAPIRequest,
+  PostAPIRequest,
+} from './../Modules/DataBaseConnection.js';
 
 export const sendCurrentConfig = async (endpoint, postData) => {
   try {
     let url = Constants.baseURL + endpoint;
     let _postData = JSON.stringify(postData);
-    var response = await DataBaseConnection.PostAPIRequest(url, _postData);
+    var response = await PostAPIRequest(url, _postData);
     return response;
   } catch (ex) {
     CustomLogger.ErrorLogger(ex);
@@ -17,7 +20,7 @@ export const fetchCurrentConfig = async (endpoint, postData) => {
   try {
     let url = Constants.baseURL + endpoint;
     let _postData = JSON.stringify(postData);
-    var response = await DataBaseConnection.PostAPIRequest(url, _postData);
+    var response = await PostAPIRequest(url, _postData);
     return response;
   } catch (ex) {
     CustomLogger.ErrorLogger(ex);
